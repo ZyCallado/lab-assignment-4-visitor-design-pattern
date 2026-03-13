24ac61bc26e82a20581e483e7e039f25a9f8dd9e
Zyrus C. Velasco <zyvel0@gmail.com>
2026-03-13 16:43:56 +0800

    Update ShippingCostCalculator.java

--- visitordesignpattern/ShippingCostCalculator.java
+++ visitordesignpattern/ShippingCostCalculator.java
@@ -4,23 +4,18 @@ public class ShippingCostCalculator implements ShippingVisitor {
     
     @Override
     public double visitChair(Chair chair, HashMap<String, Double> data) {
-
         double flatRate = data.get("chairFlatRate");
-
         return flatRate;
     }
 
     @Override
     public double visitTable(Table table, HashMap<String, Double> data) {
-
         double ratePerKg = data.get("tableRatePerKg");
-
         return table.getWeight() * ratePerKg;
     }
 
     @Override
     public double visitSofa(Sofa sofa, HashMap<String, Double> data) {
-
         double distance = data.get("distance");
         double bulkyRate = data.get("sofaRate");
 
